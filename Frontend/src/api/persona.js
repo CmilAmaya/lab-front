2d54770112d436da2067e36c8d8644e65989da12
CmilAmaya <macamayar@gmail.com>
2024-12-19 00:36:01 -0500

    redeploy 3

--- Frontend/src/api/persona.js
+++ Frontend/src/api/persona.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.VITE_API_URL; 
+const BASE_URL = "https://lab-leroi.vercel.app"; 
 
 // Obtener todas las personas
 export const getPersonas = async () => {
