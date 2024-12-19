ce53441ce82a53b2bdb2417dececb21888e46df8
CmilAmaya <macamayar@gmail.com>
2024-12-19 01:18:12 -0500

    fr2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:3000"
-  },
-})
+  build: {
+    outDir: 'dist'
+  }
+});
