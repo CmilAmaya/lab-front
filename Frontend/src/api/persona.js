aa62b07177273c6b44165eae09493ac800c2e8b4
CmilAmaya <macamayar@gmail.com>
2024-12-19 00:38:43 -0500

    redeploy 4

--- Frontend/src/api/persona.js
+++ Frontend/src/api/persona.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://lab-leroi.vercel.app"; 
+const BASE_URL = "https://lab-leroi.onrender.com"; 
 
 // Obtener todas las personas
 export const getPersonas = async () => {
